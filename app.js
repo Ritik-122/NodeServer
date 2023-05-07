4ab11f6cab351c282b70681fe939bc7e143b27f6
ritik sharma <bharadwajritik1@gmail.com>
2023-05-07 16:16:11 +0530

    "only using nodejs"

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const http=require('http');
 
 const server=http.createServer(function(req,res){
- console.log('Ritik')
+const url=req.url
+
+if(url==='/home'){
+    res.write('<html>')
+    res.write('<body><h3>Welcome Home</h3></body>')
+
+    res.write('</html>')
+    return res.end()
+}
+if(url==='/about'){
+    res.write('<html>')
+    res.write('<body><h3>Welcome about page</h3></body>')
+
+    res.write('</html>')
+    return res.end()
+}
+if(url==='/node'){
+    res.write('<html>')
+    res.write('<body><h3>Welcome to my node project</h3></body>')
+
+    res.write('</html>')
+    return res.end()
+}
+
+
+
 });
 
 server.listen(4000);
