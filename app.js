5bc8d0540fb3a0e1897a16a891caf4886d28615b
ritik sharma <bharadwajritik1@gmail.com>
2023-05-11 03:09:54 +0530

    input in console

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express=require('express');
 const app=express();
-
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
+const bodyParser=require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+   res.send("<form action='/product' method='POST'><input type='text' name='title'/><button>Add</button></form>")
+    
 })
-app.use((req,res,next)=>{
-    console.log('In another  middleware');
-    res.send('<h1>Hello Brother</h1>');
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+   res.redirect('/');
+    
+
+})
+app.use('/',(req,res,next)=>{
+    
+   
+    res.send('<h1>Home Page</h1>');
 })
 app.listen(4000);
