c5a839cb40606d1109a6e8ff0a20c02412656f46
ritik sharma <bharadwajritik1@gmail.com>
2023-05-11 10:54:44 +0530

    routes created

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const express=require('express');
 const app=express();
 const bodyParser=require('body-parser')
+const adminRoute=require('./routes/admin')
+const shopRoute=require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-   res.send("<form action='/product' method='POST'><input type='text' name='title'/><button>Add</button></form>")
-    
-})
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-   res.redirect('/');
-    
 
+app.use(adminRoute)
+app.use(shopRoute)
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
-app.use('/',(req,res,next)=>{
-    
-   
-    res.send('<h1>Home Page</h1>');
-})
+
+
 app.listen(4000);
